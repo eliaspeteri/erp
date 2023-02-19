b3f0e27d86c9d897f3d35de923dd7973e0e8f69e
Elias Peteri <elias.peteri@hotmail.com>
2023-02-19 17:06:45 +0200

    feat: `users` microservice

---
+++ users/src/app.ts
@@ -0,0 +1,27 @@
+// app.ts
+import cors, { CorsOptions } from 'cors';
+
+import express, { Application, Request, Response } from 'express';
+
+import bodyParser from 'body-parser';
+
+const app: Application = express();
+
+const corsOptions: CorsOptions = {
+  origin: 'http://localhost',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/health', (_req: Request, res: Response) => {
+  return res.status(200).json({ success: true });
+});
+
+app.get('/*', (_req: Request, res: Response) => {
+  return res.status(404).json({ success: false });
+});
+
+export default app;
