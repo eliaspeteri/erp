c84e9a06e607724032401bf65fa7c5202b6718e4
Elias Peteri <elias.peteri@hotmail.com>
2023-02-20 10:29:54 +0200

    fix: ms-script copies everything from templates

--- create-ms.sh
+++ create-ms.sh
@@ -5,6 +5,6 @@ npm init -y
 npm i body-parser cors dotenv express @prisma/client
 npm i --save-dev @types/{body-parser,cors,express,node} nodemon ts-node typescript prisma
 
-cp -r ../templates/src .
+cp -r ./templates/* .
 
 npx prisma init --datasource-provider sqlite
